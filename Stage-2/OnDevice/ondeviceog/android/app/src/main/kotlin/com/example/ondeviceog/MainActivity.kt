ea97cc5509b6d578ded606043528f35c93517855
Anish Pawar <anish.pawar@somaiya.edu>
2021-06-23 01:57:01 +0530

    [Clean Up]

--- Stage-2/OnDevice/ondeviceog/android/app/src/main/kotlin/com/example/ondeviceog/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ondeviceog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Stage-2/OnDevice/ondeviceog/ios/Pods/Target Support Files/tflite_flutter/tflite_flutter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_tflite_flutter : NSObject
-@end
-@implementation PodsDummy_tflite_flutter
-@end
